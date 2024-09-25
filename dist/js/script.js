b5d99c53ecdfa837ad34ab0959a38e7af8257253
mmajesty23 <muhammadmajesty23@gmail.com>
2024-09-25 22:40:14 +0700

    start deploy navbar for mobile

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Hamburger
+const btnHamburger = document.getElementById("hamburger");
+btnHamburger.addEventListener("click", function () {
+  btnHamburger.classList.toggle("hamburge--active");
+});
