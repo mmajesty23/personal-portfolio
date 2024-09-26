1823a38de1218fadae578118be32be2f1ce106e3
mmajesty23 <muhammadmajesty23@gmail.com>
2024-09-26 10:43:19 +0700

    Implementing hamburger menu animation and nav intersectionObserver

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,30 @@
 "use strict";
+const nav = document.getElementById("nav");
+const btnHamburger = document.getElementById("hamburger");
+const navHeight = nav.getBoundingClientRect().height;
+
+// nav fixed
+const observerCallback = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    nav.classList.add("navbar-fixed");
+  } else {
+    nav.classList.remove("navbar-fixed");
+  }
+};
+
+const observerOptions = {
+  root: null,
+  threshold: 0.5,
+  rootMargin: `-${navHeight}px`,
+};
+const headerObserver = new IntersectionObserver(
+  observerCallback,
+  observerOptions
+);
+headerObserver.observe(nav);
 
 // Hamburger
-const btnHamburger = document.getElementById("hamburger");
-btnHamburger.addEventListener("click", function () {
-  btnHamburger.classList.toggle("hamburge--active");
+btnHamburger.addEventListener("click", () => {
+  btnHamburger.classList.toggle("hamburger--active");
 });
